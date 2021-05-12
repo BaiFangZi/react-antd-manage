c207a259228d28ff5bbcb93f3b6720ce65005f33
weibin <2739666469@qq.com>
2021-05-12 19:48:02 +0800

    feat:upload excel

---
+++ src/pages/excel/uploadExcel/component/previewExcel/index.jsx
@@ -0,0 +1,21 @@
+import { Table } from 'antd'
+import { useState } from 'react'
+
+const PreviewExcel = (props) => {
+  const { excelData } = props
+  // const [tableData, setTableData] = useState([])
+  const tableData = excelData.map((i) => {
+    const { __rowNum__, ...data } = i
+    return data
+  })
+  const columns = Object.keys(tableData[0] || {}).map((i) => ({
+    title: i,
+    dataIndex: i,
+  }))
+  return (
+    <div>
+      <Table columns={columns} dataSource={tableData}></Table>
+    </div>
+  )
+}
+export default PreviewExcel
--- src/pages/excel/uploadExcel/index.jsx
+++ src/pages/excel/uploadExcel/index.jsx
@@ -1,46 +1,94 @@
-import { Upload, message } from 'antd'
+import { Upload, message, Divider } from 'antd'
 import { InboxOutlined } from '@ant-design/icons'
+import PreviewExcel from './component/previewExcel'
+import { useState } from 'react'
+import XLSX from 'xlsx'
 
 const { Dragger } = Upload
-
-// const props = {
-//   name: 'file',
-//   multiple: true,
-//   action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-//   onChange(info) {
-//     const { status } = info.file
-//     if (status !== 'uploading') {
-//       console.log(info.file, info.fileList)
-//     }
-//     if (status === 'done') {
-//       message.success(`${info.file.name} file uploaded successfully.`)
-//     } else if (status === 'error') {
-//       message.error(`${info.file.name} file upload failed.`)
-//     }
-//   },
-// }
-
-// <Dragger {...props}>
-// <p className="ant-upload-drag-icon">
-//   <InboxOutlined />
-// </p>
-// <p className="ant-upload-text">Click or drag file to this area to upload</p>
-// <p className="ant-upload-hint">
-//   Support for a single or bulk upload. Strictly prohibit from uploading
-//   company data or other band files
-// </p>
-// </Dragger>,
-
 const UploadExcel = () => {
+  const [excelData, setExcelData] = useState([])
+  const [preFileName, setPreFileName] = useState('')
+  const uploadProps = () => {
+    return {
+      name: 'file12121', //文件对象的key
+      multiple: true,
+      accept: '.xlsx, .xls, .csv', //上传文件限制
+      action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+      onChange(info) {
+        // console.log(info)
+        const { status } = info.file
+        if (status !== 'uploading') {
+          // console.log(info.file, info.fileList)
+        }
+        if (status === 'done') {
+          message.success(`${info.file.name} 文件上传成功.`)
+        } else if (status === 'error') {
+          message.error(`${info.file.name} 文件上传失败.`)
+        }
+      },
+      beforeUpload(file, fileList) {
+        //添加上传前的某些校验
+        if (!/\.(xlsx|xls|csv)$/.test(file.name)) {
+          message.error('仅支持上传.xlsx, .xls, .csv 文件')
+          return false
+        }
+      },
+      onPreview(e) {
+        const { name } = e.originFileObj
+        readerData(e.originFileObj).then(() => {
+          setPreFileName(name)
+        })
+      },
+      onRemove(e) {
+        console.log(preFileName)
+        // console.log(e)
+        const delFileName = e.name
+        if (delFileName === preFileName) {
+          setExcelData([])
+          setPreFileName('')
+        }
+        // console.log(12121)
+        // if()
+      },
+      customRequest(e) {
+        const { name } = e.file
+        readerData(e.file).then(() => {
+          e.onSuccess()
+          setPreFileName(name)
+        })
+      },
+    }
+  }
+
+  const readerData = (rawFile) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        const data = e.target.result
+        const workbook = XLSX.read(data, { type: 'binary' })
+        const worksheet = workbook.Sheets.SheetJS
+        const result = XLSX.utils.sheet_to_json(worksheet)
+        // console.log(result)
+        setExcelData(result)
+        resolve()
+      }
+      // reader.readAsArrayBuffer(rawFile)
+      reader.readAsBinaryString(rawFile)
+    })
+  }
   return (
     <div>
-      <Dragger>
+      <Dragger {...uploadProps()}>
         <p className="ant-upload-drag-icon">
           <InboxOutlined />
         </p>
         <p className="ant-upload-text">点击或拖拽文件到此处上传</p>
-        <p className="ant-upload-hint">支持单个/批量上产，谨慎上传敏感数据</p>
+        <p className="ant-upload-hint">
+          支持单个/批量上产，别上传敏感数据，文件名包含特殊字符可能导致上传失败！
+        </p>
       </Dragger>
+      {preFileName && <Divider plain>正在预览 {preFileName}</Divider>}
+      <PreviewExcel excelData={excelData}></PreviewExcel>
     </div>
   )
 }
